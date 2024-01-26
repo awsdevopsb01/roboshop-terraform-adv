8d886eae8c350a96678cc8173f2c0111db964b8a
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-01-26 20:44:25 +0530

    practice Ansible - Tuning terraform

---
+++ module/main.tf
@@ -0,0 +1,37 @@
+
+resource "aws_instance" "instance" {
+  ami = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+
+  tags = {
+    Name  = var.component_name
+  }
+}
+
+resource "aws_route53_record" "dnsrecord" {
+  zone_id = "Z01307132WU1DJMGVKGO6"
+  name    = "${var.component_name}-dev.nldevopsb01.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
+
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.dnsrecord]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-adv-shell",
+      "git clone https://github.com/awsdevopsb01/roboshop-adv-shell.git",
+      "cd roboshop-adv-shell",
+      "sudo bash ${var.component_name}.sh"
+    ]
+  }
+}
---
+++ servers.tf
@@ -0,0 +1,6 @@
+module "servers" {
+  for_each = var.components
+  source = "./module"
+  component_name = each.value["name"]
+  instance_type = each.value["instance_type"]
+}
