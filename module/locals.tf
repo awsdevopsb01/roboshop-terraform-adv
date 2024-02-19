72f3bb38b6548d3f0eb917526f57ae934b70b3a5
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-02-19 21:31:55 +0530

    prometheus config - filter option

--- module/locals.tf
+++ module/locals.tf
@@ -9,7 +9,13 @@ locals {
   app_commands = [
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/awsdevopsb01/roboshop-ansible.git roboshop.yml -e role_name=${var.component_name}"
-
   ]
 
+  db_tags = {
+    Name  = "${var.component_name}"
+  }
+  app_tags = {
+    Name  = "${var.component_name}"
+    Monitor = "true"
+  }
 }
--- module/main.tf
+++ module/main.tf
@@ -5,9 +5,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   iam_instance_profile = aws_iam_instance_profile.iam_ssm_instance_profile.name
 
-  tags = {
-    Name  = var.component_name
-  }
+  tags = var.app_type == "app" ? local.app_tags:local.db_tags
 }
 
 resource "aws_route53_record" "dnsrecord" {
